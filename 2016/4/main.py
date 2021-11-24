cbe903f8ed6247b968f61c91cde9d70fbd343e32
NimVrod <hdec0759@gmail.com>
2021-11-24 18:36:41 +0100

    2016/4 2/3
    Podpunkty 2/3 zrobione
    - Brak odczytu i wypisu do pliku

---
+++ 2016/4/main.py
@@ -0,0 +1,30 @@
+import random
+def podpunkt1(tab):
+    suma1 = 0 # na okręgu
+    suma2 = 0 # wewnątrz okręgu
+    for liczba in tab:
+        #print(liczba)
+        if ((liczba[0]-200)**2) + ((liczba[1]-200)**2) == 200**2:
+            suma1 += 1
+        if ((liczba[0]-200)**2) + ((liczba[1]-200)**2) < 200**2:
+            suma2 +=1
+    return suma1, suma2
+
+def podpunkt2(tab, nk, a=400, r=200) -> int:
+    """
+    nk/n≈Pk/P
+    """
+
+    pi = (nk[1]*(a**2))/(len(tab)*(r**2))
+    return pi
+
+
+
+tab = []
+for i in range(0, 10000):
+    tab.append([random.randint(0, 400), random.randint(0, 400)])
+tab[0] = [400, 200]
+#print("\n\n\n\n")
+n =(podpunkt1(tab))
+print(n)
+print(podpunkt2(tab, n))
