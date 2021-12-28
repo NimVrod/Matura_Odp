f5ddb4de1f8a26c695b1ad01ab597b22421b5e68
NimVrod <hdec0759@gmail.com>
2021-12-28 17:10:22 +0100

    Naprawiono 2016/6

--- 2016/6/main.py
+++ 2016/6/main.py
@@ -21,24 +21,36 @@ def odszyfruj(haslo :str, klucz):
     return new
 
 #podpunkt1
-with open("dane6_1.txt","r") as f, open("wynik6_1.txt","w") as o:
+with open("dane_6_1.txt","r") as f, open("wynik_6_1.txt","w") as o:
     for line in f:
         o.write(zaszyfruj(line.strip(), 107) + "\n")
 
 #podpunkt2
-with open("dane6_2.txt","r") as f, open("wynik6_2.txt","w") as o:
+with open("dane_6_2.txt","r") as f, open("wynik_6_2.txt","w") as o:
     for line in f:
-        tab = line.split(" ")
+        tab = line.strip().split(" ")
+        try:
+            if tab[1] == None:
+                tab[1] = 0
+        except IndexError:
+            tab.append(0)
         o.write(odszyfruj(tab[0], int(tab[1])) + "\n")
 
 #podpunkt3
-with open("dane6_3.txt","r") as f, open("wynik6_3.txt", "w") as o:
+with open("dane_6_3.txt","r") as f, open("wynik_6_3.txt", "w") as o, open("test.txt", 'w') as t:
     wyrazy = []
     for line in f:
-        tab = line.split(" ")
-        for i in range(1, 26):
+        tab = line.strip().split(" ")
+        if tab == []:
+            break
+        dobry = False
+        for i in range(0, 27):
             if tab[0] == odszyfruj(tab[1], i):
-                break
-        wyrazy.append(tab[0] + "\n")
+                dobry = True
+        
+        if dobry == False:
+            wyrazy.append(tab[0])
+        
 
-    o.writelines(wyrazy)   
+    for wyraz in wyrazy:
+        o.write(f"{wyraz}\n")   
